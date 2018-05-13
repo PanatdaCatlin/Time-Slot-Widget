50aace70da7b639331e57b76cc0b2b925242576e
Panatda Inthala <panatdainthala@gmail.com>
2018-05-12 23:40:15 -0700

    it will have a row and cols to the widget body

--- timeSlotWidget.js
+++ timeSlotWidget.js
@@ -1,3 +1,35 @@
+const widgetHeader = () =>{
+ return `<h1>Book Online</h1>
+ <a href="http://www.wheelhousetesting.net">What do we treat? </a><a href="http://www.wheelhousetesting.net">How much will it cost? </a>`;
+}
+const widgetBody = () => {
+    return `<h2>Tomorrow</h2>
+    <div class="row" style="margin:10px">
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  <div class="col-sm-3">time</div>
+  </div>`;
+}
+
+const widget = () => {
+  return (`
+  <div id='timeSlotWidget'> 
+    ${widgetHeader()}
+    <hr/>
+    ${widgetBody()}
+  </div>
+  `);
+};
+
 $(document).ready(() => {
-  $("#addContentHere").append("<p>Hello World</p>");
+  $("#addContentHere").append(widget());
 });
